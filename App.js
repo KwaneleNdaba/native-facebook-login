9da0d039e51f629bb41e3d8c872b936f548517d2
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2023-08-23 03:47:24 +0200

    last commit

--- App.js
+++ App.js
@@ -1,71 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import {getAuth, FacebookAuthProvider, signInWithCredential} from 'firebase/auth'
-import {firebase} from './config'
-import { LoginManager } from 'react-native-fbsdk-next';
-import React, {useState, useEffect} from "react"
-export default function App() {
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  getAuth,
+  FacebookAuthProvider,
+  signInWithCredential,
+} from 'firebase/auth';
+import { firebase } from './config';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next'; // Import AccessToken
 
-  const[initializing , seInitializing] = useState(true)
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
 
-  function onAuthStateChange(user){
+  function onAuthStateChanged(user) {
     setUser(user);
-    if(initializing){
-      seInitializing(false)
+    if (initializing) {
+      setInitializing(false);
     }
   }
 
   useEffect(() => {
-    const subscriber = firebase.auth.onAuthStateChanged(onAuthStateChanged)
-    return subscriber ;
-  }, []) 
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
   const signInWithFB = async () => {
     try {
-      await LoginManager.logInWithPermissions(['public_profile', "email"]);
-      const data = await AcccessToken.getCurrentToken();
-      if(!data) {
+      await LoginManager.logInWithPermissions(['public_profile']);
+      const accessToken = await AccessToken.getCurrentAccessToken();
+      if (!accessToken) {
         return;
       }
 
-      const facebookCredential = FacebookAuthProvider.credential(data.acccessToken);
+      const facebookCredential = FacebookAuthProvider.credential(
+        accessToken.accessToken
+      );
       const auth = getAuth();
       const response = await signInWithCredential(auth, facebookCredential);
-      console.log("response: ", response)
+      console.log('response: ', response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  };
 
-    const signOut = async () => {
-      try {
-        await firebase.auth().signOut();
-      } catch (error) {
-        console.log(error)
-      }
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
-  if(initializing) return null;  
+  if (initializing) return null;
 
-  if(!user){
+  if (!user) {
     return (
       <View style={styles.container}>
-        <Text style = {{fontWeight:"bold"}}>Facebook Auth with Native</Text>
-        <Button title='sign in with Facebook' onPress = {signInWithFB}/>
+        <Text style={{ fontWeight: 'bold' }}>Facebook Auth with Native</Text>
+        <Button title="Sign in with Facebook" onPress={signInWithFB} />
       </View>
-    )
+    );
   }
 
   return (
     <View style={styles.container}>
-        <Text style = {{fontWeight:"bold"}}>Facebook Auth with Native</Text>
+      <Text style={{ fontWeight: 'bold' }}>Facebook Auth with Native</Text>
       <StatusBar style="auto" />
-      <View>
-        <View>
-          <Text>Welceme {user.displayname}</Text>
-        </View>
-      </View>
+      <Text>Welcome {user.displayName}</Text> 
+      <Text>Welcome {user.email}</Text> 
     </View>
   );
 }
